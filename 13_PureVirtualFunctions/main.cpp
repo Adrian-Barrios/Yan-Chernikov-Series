29320b45b92efda2e4f3541fc0c33ad9d6759e0a
Adrian Barrios <adrianbarrioscortez@gmail.com>
2026-07-03 18:10:08 +0200

    pure virtual functions

---
+++ 13_PureVirtualFunctions/main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <string>
+
+class Printables
+{
+public:
+	virtual std::string GetClassName() = 0;
+};
+
+class Parent : public Printables
+{
+public:
+	virtual std::string GetName() = 0;
+	std::string GetClassName() override { return "Parent"; }
+};
+
+void PrintName(Parent* p) {};
+
+class Child : public Parent
+{
+private:
+	std::string m_Name;
+public:
+	Child(const std::string& name) :m_Name(name) {};
+	std::string GetName() { return "Child"; }
+	std::string GetClassName() override { return "Child"; }
+};
+
+void Print(Printables* obj)
+{
+	std::cout << obj->GetClassName() << std::endl;
+}
+
+int main()
+{
+	Parent* p = new Parent();
+	std::cout << p->GetClassName() << std::endl;
+
+	Child* c = new Child("Adrian");
+	std::cout << c->GetName() << std::endl;
+
+	Print(c);
+}
