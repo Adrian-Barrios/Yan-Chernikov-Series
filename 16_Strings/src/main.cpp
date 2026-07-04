6be05913d25161cd0a8622260822d1a4fb8460b4
Adrian Barrios <adrianbarrioscortez@gmail.com>
2026-07-04 21:26:18 +0200

    We can use std::string, c-style strings (as a pointer to an array), or create an array of characters (and specify the null terminator). A std::string constructs using a const char[].
    If we want to add two strings in the same line, we need to cast one of them to a std:.string, because if not, you are trying to add const char[], which is not possible

---
+++ 16_Strings/src/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+	// GENERAL IDEA
+
+	std::string MY_STRING = std::string("SOME STRING ") + "Plus Something else";
+	bool contains = MY_STRING.find("Something") != std::string::npos;
+
+	MY_STRING[0] = 'T';
+	int string_size = MY_STRING.size();
+
+	std::cout << MY_STRING << std::endl;
+
+	// UNDERLYING CHARACTERS (BUILDING BLOCKS OF STRINGS)
+
+	char MY_CHAR = 'c';
+	MY_CHAR = 'i';
+
+	std::cout << MY_CHAR << std::endl;
+
+	// C-Style String
+
+	const char* MY_NAME = "ADRIAN"; // no need to call delete
+	MY_NAME = "SOMETHING ELSE"; // I can change it entirely, but not the specific chars
+
+	std::cout << MY_NAME << std::endl;
+
+	// Array Representation
+
+	char MY_NAME_2[6] = { 'H', 'e', 'l', 'l', 'o', '\0'}; // We need this null terminator explicitly declared if we do things this way.
+	std::cout << MY_NAME_2 << std::endl;
+}
