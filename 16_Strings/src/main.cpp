746ac148f1ff49cb29e8bb23b8304f93773a0f69
Adrian Barrios <adrianbarrioscortez@gmail.com>
2026-07-04 22:15:58 +0200

    added explanations as to "string"s, which turns a cstyle string into a std::string. Also how to write a multi-line string: const char* multi_line_string = R"(line 1 line 2 line 3)"

--- 16_Strings/src/main.cpp
+++ 16_Strings/src/main.cpp
@@ -1,12 +1,20 @@
 #include <iostream>
 #include <string>
 
+using namespace std::string_literals; // for things like "string"s, which turns a cstyle string into a std::string
+
 int main()
 {
 	// GENERAL IDEA
 
 	std::string MY_STRING = std::string("SOME STRING ") + "Plus Something else";
+	std::string MY_OTHER_STRING = "SOME OTHER STRING "s + "Plus Something else";
 	bool contains = MY_STRING.find("Something") != std::string::npos;
+
+	const char* MULTI_LINE_STRING = R"(line 1
+line 2
+line 3
+line 4)";
 
 	MY_STRING[0] = 'T';
 	int string_size = MY_STRING.size();
