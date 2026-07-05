c253cadd5bb606efcd2df28592cefe7e84da2478
Adrian Barrios <adrianbarrioscortez@gmail.com>
2026-07-05 14:40:11 +0200

    running into linking errors with glfw3.lib

---
+++ 26_StaticLinkingBinaries/src/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <glfw3.h>
+
+int main()
+{
+	int a = glfwInit();
+
+}
+
+/*
+GLFW provides precompiled binaries for things like OpenGL
+
+There are 2 things that need to happen.
+First, by including a header file, it means you can compile, but not necessarily link.
+
+To link, you need a function's definition.
+
+*/
