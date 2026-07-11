9cb91b801ef48669cfa6ae54d7ca20102cf160f7
Adrian Barrios <adrianbarrioscortez@gmail.com>
2026-07-11 12:51:42 +0200

    std::string_view allows for no allocation when trying to interact with pieces of an original string. Additionally, by using const char*, we avoid the allocation that happens with std::string

---
+++ 50_FasterStrings/src/main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <string>
+
+// String view came in C++17 (basically a const char* to existing text)
+
+static uint32_t s_AllocCount = 0;
+
+void* operator new(size_t size) 
+{
+	s_AllocCount++;
+	std::cout << "Allocating: " << size << " bytes\n";
+	return malloc(size);
+}
+
+void PrintName(const std::string& s)
+{
+	std::cout << s << std::endl;
+}
+
+void PrintViewName(std::string_view s)
+{
+	std::cout << s << std::endl;
+}
+
+int main()
+{
+#if 0
+	std::string name = "Adrian Barrios"; // 1 allocation in debug mode
+	std::string firstName = name.substr(0, 6); // 2nd allocation
+	std::string lastName = name.substr(7, 14); // 3rd allocation
+	std::cout << s_AllocCount << " allocations" << std::endl;
+
+#else 
+	const char* nameForView = "Adrian Barrios"; // We use this for string views. No allocation
+	std::string_view firstNameView(nameForView, 6); // no allocation
+	std::string_view lastNameView(nameForView + 7, 14); // no allocation
+	std::cout << s_AllocCount << " allocations" << std::endl;
+
+#endif
+
+	
+
+	std::cin.get();
+}
