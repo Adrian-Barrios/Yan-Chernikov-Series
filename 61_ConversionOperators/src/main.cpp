15750cec7aa4ba500f60f2338d2cd58bfe62973c
Adrian Barrios <adrianbarrioscortez@gmail.com>
2026-07-12 14:18:22 +0200

    We can delcare conversion operators. For example, if we are casting a type into another type and we want to control the behavior.
     operator bool() const { return m_Ptr != nullptr; }
    
    In this case, if we cast the object to a bool, it will return true if m_Ptr is not null and false if it is null.

---
+++ 61_ConversionOperators/src/main.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <memory>
+
+template<typename T>
+class ScopedPtr
+{
+public:
+	ScopedPtr() = default;
+	ScopedPtr(T* ptr) : m_Ptr(ptr) {}
+	~ScopedPtr() {delete m_Ptr}
+	
+	T* Get() { return m_Ptr; }
+	const T* Get() const { return m_Ptr; }
+private:
+	T* m_Ptr = nullptr;
+	operator bool() const { return m_Ptr != nullptr; }
+};
+
+struct Orange
+{
+	operator float() { return 5.5f; }
+};
+
+struct Entity
+{
+	float X = 0.0f, Y = 0.0f;
+};
+
+void ProcessEntity(Entity* e)
+{
+	if (e)
+	{
+		// ... 
+	}
+}
+
+int main()
+{
+	Orange orange;
+	float f = orange;
+
+	ScopedPtr<Entity> e = new Entity();
+	Entity* entity = new Entity();
+
+	if (entity)
+
+	std::cout << (float)orange << std::endl;
+
+}
